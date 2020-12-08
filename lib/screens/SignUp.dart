f8e83f1a53ea07f127c343cdfdc3f82c0bda3fb1
Harshana ratnayaka <huratnayaka@gmail.com>
2020-12-08 17:10:46 +0530

    user registration done

--- lib/screens/SignUp.dart
+++ lib/screens/SignUp.dart
@@ -5,9 +5,9 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:vehicle_management_system/constants/images.dart';
 import 'package:http/http.dart' as http;
-import 'package:vehicle_management_system/screens/AdminHome.dart';
-import 'package:vehicle_management_system/screens/UserHome.dart';
+import 'package:vehicle_management_system/screens/LoginPage.dart';
 import 'package:vehicle_management_system/services/NetworkHelper.dart';
+import 'package:vehicle_management_system/widgets/MyTextField.dart';
 
 class SignUp extends StatefulWidget {
   @override
@@ -21,18 +21,20 @@ class _SignUpState extends State<SignUp> {
   bool _loading = false;
 
   TextEditingController _usernameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  Future<http.Response> _login() async {
+  Future<http.Response> _register() async {
     setState(() {
       _loading = true;
     });
 
     final http.Response response = await Network().postData({
       'username': _usernameController.text,
+      'email': _emailController.text,
       'password': _passwordController.text
-    }, '/userLogin.php');
+    }, '/registerUser.php');
 
     print(jsonDecode(response.body));
 
@@ -41,6 +43,19 @@ class _SignUpState extends State<SignUp> {
     });
 
     return response;
+  }
+
+  String validateEmail(String value) {
+    String pattern =
+        r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
+    RegExp regExp = new RegExp(pattern);
+
+    if (value.isEmpty) {
+      return 'Email address is required';
+    } else if (!regExp.hasMatch(value)) {
+      return 'Please enter a valid email address';
+    }
+    return null;
   }
 
   @override
@@ -62,96 +77,138 @@ class _SignUpState extends State<SignUp> {
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
                           Text(
-                            'LOGIN',
+                            'REGISTER',
                             style: TextStyle(
                                 fontWeight: FontWeight.bold, fontSize: 28),
                           ),
                           SvgPicture.asset(
                             login_image,
-                            height: height * 0.35,
+                            height: width * 0.50,
                           ),
                           SizedBox(height: 20),
-                          // username field
-                          Container(
-                            width: width * 0.85,
-                            padding: const EdgeInsets.symmetric(horizontal: 20),
-                            margin: const EdgeInsets.symmetric(
-                                horizontal: 20, vertical: 10),
-                            decoration: BoxDecoration(
-                                color: Colors.teal[100],
-                                borderRadius: BorderRadius.circular(20)),
-                            child: TextFormField(
-                              validator: (val) {
-                                if (val.isEmpty) {
-                                  return 'Please enter your username';
-                                }
-                                return null;
-                              },
-                              cursorColor: Colors.teal,
-                              controller: _usernameController,
-                              decoration: InputDecoration(
-                                icon: Icon(
-                                  Icons.person,
-                                  color: Colors.teal,
-                                ),
-                                hintText: 'Username',
-                                hintStyle:
-                                    TextStyle(color: Colors.blueGrey[700]),
-                                border: InputBorder.none,
-                              ),
-                            ),
+                          // // username field
+
+                          // // email field
+                          // Container(
+                          //   width: width * 0.85,
+                          //   padding: const EdgeInsets.symmetric(horizontal: 20),
+                          //   margin: const EdgeInsets.symmetric(
+                          //       horizontal: 20, vertical: 10),
+                          //   decoration: BoxDecoration(
+                          //       color: Colors.teal[100],
+                          //       borderRadius: BorderRadius.circular(20)),
+                          //   child: TextFormField(
+                          //     keyboardType: TextInputType.emailAddress,
+                          //     validator: (val) {
+                          //       return validateEmail(val);
+                          //     },
+                          //     cursorColor: Colors.teal,
+                          //     controller: _emailController,
+                          //     decoration: InputDecoration(
+                          //       icon: Icon(
+                          //         Icons.email,
+                          //         color: Colors.teal,
+                          //       ),
+                          //       hintText: 'Email',
+                          //       hintStyle:
+                          //           TextStyle(color: Colors.blueGrey[700]),
+                          //       border: InputBorder.none,
+                          //     ),
+                          //   ),
+                          // ),
+
+                          // // password field
+                          // Container(
+                          //   width: width * 0.85,
+                          //   padding: const EdgeInsets.symmetric(horizontal: 20),
+                          //   margin: const EdgeInsets.symmetric(
+                          //       horizontal: 20, vertical: 10),
+                          //   decoration: BoxDecoration(
+                          //       color: Colors.teal[100],
+                          //       borderRadius: BorderRadius.circular(20)),
+                          //   child: TextFormField(
+                          //     validator: (val) {
+                          //       if (val.isEmpty) {
+                          //         return 'Password is required';
+                          //       }
+                          //       return null;
+                          //     },
+                          //     cursorColor: Colors.teal,
+                          //     controller: _passwordController,
+                          //     obscureText: visible ? false : true,
+                          //     decoration: InputDecoration(
+                          //       suffixIcon: GestureDetector(
+                          //         onTap: () {
+                          //           setState(() {
+                          //             visible = !visible;
+                          //           });
+                          //         },
+                          //         child: Icon(
+                          //           visible
+                          //               ? Icons.visibility
+                          //               : Icons.visibility_off,
+                          //           color: Colors.teal,
+                          //         ),
+                          //       ),
+                          //       icon: Icon(
+                          //         Icons.lock,
+                          //         color: Colors.teal,
+                          //       ),
+                          //       hintText: 'Password',
+                          //       hintStyle:
+                          //           TextStyle(color: Colors.blueGrey[700]),
+                          //       border: InputBorder.none,
+                          //     ),
+                          //   ),
+                          // ),
+
+                          // username
+                          MyTextField(
+                            controller: _usernameController,
+                            hint: "Username",
+                            width: width,
+                            icon: Icons.person,
+                            validation: (val) {
+                              if (val.isEmpty) {
+                                return "Username is required";
+                              }
+                              return null;
+                            },
                           ),
 
-                          // password field
-                          Container(
-                            width: width * 0.85,
-                            padding: const EdgeInsets.symmetric(horizontal: 20),
-                            margin: const EdgeInsets.symmetric(
-                                horizontal: 20, vertical: 10),
-                            decoration: BoxDecoration(
-                                color: Colors.teal[100],
-                                borderRadius: BorderRadius.circular(20)),
-                            child: TextFormField(
-                              validator: (val) {
-                                if (val.isEmpty) {
-                                  return 'Please enter your password';
-                                }
-                                return null;
-                              },
-                              cursorColor: Colors.teal,
-                              controller: _passwordController,
-                              obscureText: visible ? false : true,
-                              decoration: InputDecoration(
-                                suffixIcon: GestureDetector(
-                                  onTap: () {
-                                    setState(() {
-                                      visible = !visible;
-                                    });
-                                  },
-                                  child: Icon(
-                                    visible
-                                        ? Icons.visibility
-                                        : Icons.visibility_off,
-                                    color: Colors.teal,
-                                  ),
-                                ),
-                                icon: Icon(
-                                  Icons.lock,
-                                  color: Colors.teal,
-                                ),
-                                hintText: 'Password',
-                                hintStyle:
-                                    TextStyle(color: Colors.blueGrey[700]),
-                                border: InputBorder.none,
-                              ),
-                            ),
+                          // email
+                          MyTextField(
+                            controller: _emailController,
+                            hint: "Email",
+                            width: width,
+                            isEmail: true,
+                            icon: Icons.email,
+                            validation: (val) {
+                              return validateEmail(val);
+                            },
+                          ),
+
+                          // password
+                          MyTextField(
+                            controller: _passwordController,
+                            hint: "Password",
+                            width: width,
+                            isPassword: true,
+                            isSecure: true,
+                            icon: Icons.lock,
+                            validation: (val) {
+                              if (val.isEmpty) {
+                                return "Password is required";
+                              }
+                              return null;
+                            },
                           ),
 
                           // login button
                           GestureDetector(
                             onTap: () {
                               if (_formKey.currentState.validate()) {
-                                _login().then((value) {
+                                _register().then((value) {
                                   var res = jsonDecode(value.body);
 
                                   if (res['error'] == true) {
@@ -160,19 +217,17 @@ class _SignUpState extends State<SignUp> {
                                         backgroundColor: Colors.red[600],
                                         textColor: Colors.white,
                                         toastLength: Toast.LENGTH_LONG);
-                                  } else if (res['error'] == false &&
-                                      res['super_user'] == true) {
+                                  } else if (res['error'] == false) {
+                                    Fluttertoast.showToast(
+                                        msg: res['message'],
+                                        backgroundColor: Colors.green,
+                                        textColor: Colors.white,
+                                        toastLength: Toast.LENGTH_LONG);
                                     Navigator.pushReplacement(
                                         context,
                                         MaterialPageRoute(
                                             builder: (BuildContext context) =>
-                                                AdminHomePage()));
-                                  } else {
-                                    Navigator.pushReplacement(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (BuildContext context) =>
-                                                UserHomePage()));
+                                                LoginPage()));
                                   }
                                 });
                               }
@@ -181,7 +236,7 @@ class _SignUpState extends State<SignUp> {
                               width: width * 0.85,
                               height: 45,
                               child: Text(
-                                'LOGIN',
+                                'SIGNUP',
                                 style: TextStyle(
                                     color: Colors.white,
                                     fontSize: 20,
@@ -204,7 +259,7 @@ class _SignUpState extends State<SignUp> {
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
                               Text(
-                                'Don\'t have an account?',
+                                'Already have an account?',
                                 style:
                                     TextStyle(color: Colors.teal, fontSize: 16),
                               ),
@@ -212,8 +267,14 @@ class _SignUpState extends State<SignUp> {
                                 width: 5,
                               ),
                               GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (_) => LoginPage()));
+                                },
                                 child: Text(
-                                  'Sign up',
+                                  'Log in',
                                   style: TextStyle(
                                       color: Colors.teal,
                                       fontWeight: FontWeight.bold,

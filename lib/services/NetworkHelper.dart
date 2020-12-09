df37ae1a1384d2ec4bbf135381e8f9f7a352beea
Harshana ratnayaka <huratnayaka@gmail.com>
2020-12-10 02:17:09 +0530

    added the post data method

--- lib/services/NetworkHelper.dart
+++ lib/services/NetworkHelper.dart
@@ -2,12 +2,18 @@ import 'package:connectivity/connectivity.dart';
 import 'package:http/http.dart' as http;
 
 class Network {
-  final String url = "http://0.0.0.0:8000";
+  final String url = "http://192.168.8.100/vms/api";
 
   postData(values, endpoint) async {
     var fullUrl = url + endpoint;
 
     return await http.post(fullUrl, body: values);
+  }
+
+  getData(endpoint) async {
+    var fullUrl = url + endpoint;
+
+    return await http.get(fullUrl);
   }
 
   checkInternetConnection() async {

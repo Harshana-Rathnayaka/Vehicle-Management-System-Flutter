94354640ca11815f7d881d37b947036f7ae7a6c5
Harshana ratnayaka <huratnayaka@gmail.com>
2020-12-09 21:24:52 +0530

    additional colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -2,3 +2,6 @@ import 'package:flutter/material.dart';
 
 const primaryColor = Color(0xFF008080);
 const backgroundColor = Color(0xFFF0F0F0);
+const appBarColor = Color(0xFFDBD9D9);
+const colorGrey = Color(0xFFADACAC);
+const cardColor = Color(0xFFDFD7D7);
